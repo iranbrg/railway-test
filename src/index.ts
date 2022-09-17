fcb637214449d1ba003f16e575dd6d2dc5e7ed71
Iran Braga <iran.brg@gmail.com>
2022-09-17 08:28:58 -0300

    feat: add cron job

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import axios from "axios";
 import "dotenv/config";
+var cron = require('node-cron');
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 const API_URL = process.env.API_URL ?? `http://localhost:${PORT}`;
+
+cron.schedule('*/2 * * * *', () => {
+  console.log('running a task every 2 minutes');
+});
 
 app.get("/", async (req, res) => {
     const response = await axios.get("https://api.github.com/users/iranbrg");
