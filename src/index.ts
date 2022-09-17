91db2447dc64ee5d750e31fe4d167b2336e76720
Iran Braga <iran.brg@gmail.com>
2022-09-17 06:37:30 -0300

    chore: add env vars

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import axios from "axios";
+import "dotenv/config";
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
+const API_URL = process.env.API_URL ?? `http://localhost:${PORT}`;
 
 app.get("/", async (req, res) => {
     const response = await axios.get("https://api.github.com/users/iranbrg");
     res.status(200).json({ githubData: response.data });
 })
 
-app.listen(PORT, () => console.log(`Server's running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server's running at ${API_URL}`));
